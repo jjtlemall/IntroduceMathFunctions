3fa54c81d8a06f6261ab0f0e40b348d7dfe51386
Ashley Shelton <63636009+jjtlemall@users.noreply.github.com>
2022-08-03 14:59:11 -0500

    math func beginning

---
+++ src/MathDemo.java
@@ -0,0 +1,32 @@
+public class MathDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.cos(Math.PI / 6));
+        System.out.println(Math.toRadians(30));
+        System.out.println(Math.toDegrees(0.52));
+
+        System.out.println(Math.exp(1));
+        System.out.println(Math.log(Math.E));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.sqrt(25));
+
+        System.out.println(Math.ceil(12.2));
+        System.out.println(Math.floor(12.2));
+        System.out.println(Math.rint(14.6));
+        System.out.println(Math.round(14.5));
+
+        System.out.println(Math.max(2, 5));
+        System.out.println(Math.min(2, 5));
+        System.out.println(Math.abs(42.5));
+
+        System.out.println(Math.random());
+        System.out.println((int) (1 + Math.random() * 10.0));
+
+
+
+
+    }
+}
+
+
---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.cos(Math.PI / 6));
+        System.out.println(Math.toRadians(30));
+        System.out.println(Math.toDegrees(0.52));
+
+        System.out.println(Math.exp(1));
+        System.out.println(Math.log(Math.E));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.sqrt(25));
+
+        System.out.println(Math.ceil(12.2));
+    }
+}
